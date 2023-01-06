cf8abb9ee679ad41124265338203c1f9d3812092
Alexrules237 <90267819+Alexrules237@users.noreply.github.com>
2023-01-05 21:14:36 -0500

    Swipe.cs
    Code and canvas was used from https://www.youtube.com/watch?v=xBxvqxsxdR8&ab_channel=Olaganyansima
    
    Title: Unity Tutorials - How to make swipe snap menu in Unity3d ( Horizontal Scroll) #unity #swipe
    Artist: Olaganyansima
    Publish Date: May 26, 2020
    
    -Removed all code that was not necessary to the scrolling of the levels.

--- DeMyth/Assets/[SCRIPTS]/UI/Swipe.cs
+++ DeMyth/Assets/[SCRIPTS]/UI/Swipe.cs
@@ -10,99 +10,38 @@ public class Swipe : MonoBehaviour
     
     private float scroll_pos = 0;
     float[] pos;
-    private bool runIt = false;
     private float time;
     private Button takeTheBtn;
     int btnNumber;
 
-    void Update()
-    {
+    void Update(){
         pos = new float[transform.childCount];
         float distance = 1f / (pos.Length - 1f);
 
-        if (runIt)
-        {
-            GecisiDuzenle(distance, pos, takeTheBtn);
-            time += Time.deltaTime;
-
-            if (time > 1f)
-            {
-                time = 0;
-                runIt = false;
-            }
-        }
-
-        for (int i = 0; i < pos.Length; i++)
-        {
+        for (int i = 0; i < pos.Length; i++){
             pos[i] = distance * i;
         }
 
-        if (Input.GetMouseButton(0))
-        {
+        if (Input.GetMouseButton(0)){
             scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
         }
-        else
-        {
-            for (int i = 0; i < pos.Length; i++)
-            {
-                if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
-                {
+        else{ //Snaps level selection buttons while scrolling
+            for (int i = 0; i < pos.Length; i++){
+                if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2)){
                     scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i], 0.1f);
                 }
             }
         }
 
-
-        for (int i = 0; i < pos.Length; i++)
-        {
-            if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
-            {
+        for (int i = 0; i < pos.Length; i++){ //Scales level selection buttons to appear larger when highlighted
+            if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2)){
                 transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
-                for (int j = 0; j < pos.Length; j++)
-                {
-                    if (j != i)
-                    {
+                for (int j = 0; j < pos.Length; j++){
+                    if (j != i){
                         transform.GetChild(j).localScale = Vector2.Lerp(transform.GetChild(j).localScale, new Vector2(0.8f, 0.8f), 0.1f);
                     }
                 }
             }
         }
-
-
     }
-
-    private void GecisiDuzenle(float distance, float[] pos, Button btn)
-    {
-        for (int i = 0; i < pos.Length; i++)
-        {
-            if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
-            {
-                scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[btnNumber], 1f * Time.deltaTime);
-            }
-        }
-
-        //for (int i = 0; i < btn.transform.parent.transform.childCount; i++)
-        //{
-           //btn.transform.name = ".";
-        //}
-
-    }
-    public void WhichBtnClicked(Button btn)
-    {
-        btn.transform.name = "clicked";
-        for (int i = 0; i < btn.transform.parent.transform.childCount; i++)
-        {
-            if (btn.transform.parent.transform.GetChild(i).transform.name == "clicked")
-            {
-                btnNumber = i;
-                takeTheBtn = btn;
-                time = 0;
-                scroll_pos = (pos[btnNumber]);
-                runIt = true;
-            }
-        }
-
-       
-    }
-
 }

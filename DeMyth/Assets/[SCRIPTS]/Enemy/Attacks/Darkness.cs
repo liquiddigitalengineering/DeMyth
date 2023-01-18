c183d40304bd7b44f5f885cf2c62ee888bffade1
Alexu7053 <alexumilan@gmail.com>
2023-01-18 19:30:44 +0100

    Enemy darkness attack

--- DeMyth/Assets/[SCRIPTS]/Enemy/Attacks/Darkness.cs
+++ DeMyth/Assets/[SCRIPTS]/Enemy/Attacks/Darkness.cs
@@ -12,9 +12,7 @@ public class Darkness : EnemyBaseState
 
     public override IEnumerator EnterState(EnemyStateManager enemyStateManager, int time)
     {
-        Debug.Log("Hello from darkness");
-        Light2D light = enemyStateManager.GetLight2D;
-
+        enemyStateManager.LightFade(false, 1f);
 
         yield return new WaitForSeconds(attackDuration);
         ExitState(enemyStateManager);
@@ -28,29 +26,7 @@ public class Darkness : EnemyBaseState
 
     public override void ExitState(EnemyStateManager enemyStateManager)
     {
-        Light2D light = enemyStateManager.GetLight2D;
-
-        EnableLight(light);
         enemyStateManager.SwitchToIdle(timeBeforeNextAttack);
-    }
-
-
-    private IEnumerator EnableLight(Light2D light2D)
-    {
-        while (light2D.intensity < 1 ) {
-            yield return new WaitForSeconds(0.5f);
-            light2D.intensity += 0.1f + Time.deltaTime;
-            Debug.Log("Hello?");
-        }      
-    }
-
-    private IEnumerator DisableLight(Light2D light2D)
-    {
-        Debug.Log("Hello?");
-        while (light2D.intensity > 0) {
-            yield return new WaitForSeconds(0.5f);
-            Debug.Log("Hello?");
-            light2D.intensity -= 0.1f + Time.deltaTime;
-        }
+        enemyStateManager.LightFade(true, 1f);
     }
 }
